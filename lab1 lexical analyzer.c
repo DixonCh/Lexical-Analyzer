7d9077a139c49e1822653cd35708953d94605ff3
DixonCh <dixonchaudhary7@gmail.com>
2020-04-12 13:42:16 +0545

    Update lab1 lexical analyzer.c

--- lab1 lexical analyzer.c
+++ lab1 lexical analyzer.c
@@ -5,8 +5,10 @@
 int main()
 {
     char string[5];
+    
     printf("Enter any operator symbol:");
     gets(string);
+    
     switch(string[0])
     {
         case'<':if(string[1]=='=')
@@ -14,36 +16,45 @@ int main()
         else
             printf("less than");
         break;
+            
         case'>':if(string[1]=='=')
         printf("greater than or equal to");
         else
             printf("greater than");
         break;
+            
         case'=':if(string[1]=='=')
         printf("assign to");
         else
             printf("equal to");
         break;
+            
         case'!':if(string[1]=='=')
         printf("not operator");
         else
             printf("not equal to");
         break;
+            
         case'&':
         printf("and operator");
         break;
+            
         case'%':
         printf("modulus operator");
         break;
+            
         case'/':
         printf("division operator");
         break;
+            
         case'+':
         printf("addition operator ");
         break;
+            
         case'-':
         printf("subtraction operator");
         break;
+            
         case'*':
         printf("multiplication  operator");
         break;
@@ -53,5 +64,4 @@ int main()
             
     }
     getch();
-
 }
